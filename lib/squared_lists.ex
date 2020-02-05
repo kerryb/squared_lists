5ca99050472e8f4dfd66eefba01c0020e6d95544
Kerry Buckley <kerryjbuckley@gmail.com>
2020-02-05 18:16:56 +0000

    Extract square/1 function

--- lib/squared_lists.ex
+++ lib/squared_lists.ex
@@ -1,5 +1,5 @@
 defmodule SquaredLists do
-  def comp(a, b) do
-    a |> Enum.map(&(&1 * &1)) |> Enum.sort() == b |> Enum.sort()
-  end
+  def comp(a, b), do: a |> Enum.map(&square/1) |> Enum.sort() == b |> Enum.sort()
+
+  defp square(a), do: a * a
 end
