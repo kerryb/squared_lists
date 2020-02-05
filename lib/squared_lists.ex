0779aab21b05ff931a926334cde37d26e39ac3c8
Kerry Buckley <kerryjbuckley@gmail.com>
2020-02-05 18:16:07 +0000

    Basic positive and negative tests passing

--- lib/squared_lists.ex
+++ lib/squared_lists.ex
@@ -1,2 +1,5 @@
 defmodule SquaredLists do
+  def comp(a, b) do
+    a |> Enum.map(&(&1 * &1)) |> Enum.sort() == b |> Enum.sort()
+  end
 end
--- test/squared_lists_test.exs
+++ test/squared_lists_test.exs
@@ -1,3 +1,17 @@
 defmodule SquaredListsTest do
   use ExUnit.Case
+
+  describe "SquaredLists.comp/2" do
+    test "returns true when one list consists of the squares of the other" do
+      a = [121, 144, 19, 161, 19, 144, 19, 11]
+      b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+      assert SquaredLists.comp(a, b)
+    end
+
+    test "returns false when one list DOES NOT consist of the squares of the other" do
+      a = [121, 144, 19, 161, 19, 144, 19, 11]
+      b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
+      refute SquaredLists.comp(a, b)
+    end
+  end
 end
