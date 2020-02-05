db84cb221fe9489a5618d835d924cda7071146e7
Kerry Buckley <kerryjbuckley@gmail.com>
2020-02-05 15:59:38 +0000

    New project with README

---
+++ lib/squared_lists.ex
@@ -0,0 +1,18 @@
+defmodule SquaredLists do
+  @moduledoc """
+  Documentation for `SquaredLists`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SquaredLists.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/squared_lists_test.exs
@@ -0,0 +1,8 @@
+defmodule SquaredListsTest do
+  use ExUnit.Case
+  doctest SquaredLists
+
+  test "greets the world" do
+    assert SquaredLists.hello() == :world
+  end
+end
